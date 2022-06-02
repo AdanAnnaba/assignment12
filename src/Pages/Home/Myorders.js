0b2c3db00ddf436e6c64b3057d5e2e733ff25ae1
Nahid Ahmmed Likhon <lemonnahid19@gmail>
2022-06-03 00:47:01 +0600

    new

--- src/Pages/Home/Myorders.js
+++ src/Pages/Home/Myorders.js
@@ -34,12 +34,7 @@ const Myorders = () => {
     },[user])
 
     const deleteID = id=>{
-      fetch(`https://salty-waters-02832.herokuapp.com/booking/${id}`,{
-        method: 'DELETE',
-        headers: {
-          authorization: `Bearer ${localStorage.getItem('accesstoken')}`
-        }
-      })
+      axios.delete(`https://salty-waters-02832.herokuapp.com/user${id}`,{authorization: `Bearer ${localStorage.getItem('accesstoken')}`})
       .then(res=>{
         const data = res.data;
         console.log(data);
