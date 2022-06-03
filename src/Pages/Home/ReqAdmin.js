c8134d7808770c4f7dd1691fcc3066b45939a148
Nahid Ahmmed Likhon <lemonnahid19@gmail>
2022-06-03 09:45:26 +0600

    new

--- src/Pages/Home/ReqAdmin.js
+++ src/Pages/Home/ReqAdmin.js
@@ -14,7 +14,7 @@ const ReqAdmin = ({children}) => {
         return <Loading></Loading>
     }
     
-    if(!user || admin){
+    if(!user || !admin){
         return <Navigate to='/login' state={{from: location}} replace></Navigate>
     }
     return children;
